0a34510c29b9e06d6554cc0cf8041925e2e90a7b
scrpnprdctn <scrpnprdctn@protonmail.com>
2021-02-07 17:31:31 +0100

    Clean the PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,13 +8,14 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index(){
-        $post = Post::get();
-        return view('welcome',compact('post'));
+        return view('welcome', [
+            'post' => Post::get()
+        ]);
     }
 
     public function show($slug){
-        $post = Post::where('slug', $slug)->firstOrFail();
-
-        return view('post', compact("post"));
+        return view('post', [
+            'post' => Post::where('slug', $slug)->firstOrFail()
+        ]);
     }
 }
